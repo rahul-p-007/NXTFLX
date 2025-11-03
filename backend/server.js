a395ff777780fd867544e4d4ab15e68443b3d26f
rahul-p-007 <rahulp00rtx@gmail.com>
2025-11-03 21:16:09 +0530

    Implement movie-related API endpoints and TMDB service integration

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,12 @@
 import express from "express";
 import authRouter from "./routes/auth.routes.js";
+import movieRoutes from "./routes/movie.routes.js";
 import { connect } from "./db/connect/db_connect.js";
+import { ENV_VARS } from "./config/envVars.js";
 const app = express();
 app.use(express.json()); //will allow us to parse
 
 app.use("/api/v1/auth", authRouter);
+app.use("/api/v1/movie", movieRoutes);
 
 connect(app);
